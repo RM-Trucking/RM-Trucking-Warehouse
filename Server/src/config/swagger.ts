34c24047aceb862cb8ac08ddd64a63a3328dc9ff
Prakash-SS1556 <prakash.sa@srinsofttech.com>
2026-03-11 11:28:12 +0530

    Swagger documentation and server configuration updates for R&M Trucking Warehouse Backend API.

--- Server/src/config/swagger.ts
+++ Server/src/config/swagger.ts
@@ -2,7 +2,7 @@ export const swaggerOptions = {
     definition: {
         openapi: '3.0.0',
         info: {
-            title: 'R&M Trucking Backend API',
+            title: 'R&M Trucking - Warehouse Application - Backend API',
             version: '1.0.0',
             description: 'API documentation for R&M Trucking Backend',
         },
